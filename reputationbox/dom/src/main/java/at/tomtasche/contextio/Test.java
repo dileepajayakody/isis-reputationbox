55c1e96a486c6381b5c776ee30aa4bdb5a85bd44
Dileepa Jayakody <dileepajayakody@gmail.com>
2014-05-11 00:38:57 +0530

    adding context.io classes to dom

---
+++ reputationbox/dom/src/main/java/at/tomtasche/contextio/Test.java
@@ -0,0 +1,26 @@
+package at.tomtasche.contextio;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * @author Thomas Taschauer | tomtasche.at
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		//ContextIO dokdok = new ContextIO("YOURKEY", "YOURSECRET");
+		
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("since", "0");
+		
+		//System.out.println(dokdok.allMessages("tomtasche@gmail.com", params).rawResponse.getBody());
+		
+		ContextIO gdcDemo = new ContextIO("65kd0b3k", "CetIiO0Ke0Klb2u8");
+		System.out.println(gdcDemo.allMessages("gdc2013demo@gmail.com", params));
+		//ContextIOResponse mailResponse = gdcDemo.allMessages("gdc2013demo@gmail.com", params);
+		
+	}
+}
